19d4856b041fa6655f58f19829c1dfb0d62038ba
AMYNA-7 <amyniane591@gmail.com>
2019-05-14 14:14:44 +0200

    ajout de dossiers

---
+++ laampe/java.js
@@ -0,0 +1,10 @@
+
+function bulb() {
+	document.getElementById("eteindre").src="/home/amyniane/Bureau/AMYNA-7/laampe/on.jpg"
+ 	
+}
+//fonction pour allumer
+function bulboff() {
+	document.getElementById("eteindre").src="/home/amyniane/Bureau/AMYNA-7/laampe/off.jpg"
+}
+ 	
